7cb3edf74393aff453969c539b6d96ce3b2affa4
sumoncse19 <mdsumoncse19@gmail.com>
2023-11-03 02:50:59 +0600

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </>
+  );
+}
+
+export default App;
